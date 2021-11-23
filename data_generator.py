99d0674ccaee79621d6599d9aeebfe23cc62ace0
afroozrahmati <afr.rahmati@gmail.com>
2021-11-22 20:48:21 -0500

    initial commit for data_generator.py

---
+++ data_generator.py
@@ -0,0 +1,47 @@
+from preprocessing import *
+import glob
+import numpy as np
+import pickle
+
+
+preprocess = preprocessing()
+
+pathnormal= './data/physionet/normal/'
+pathabnormal = './data/physionet/abnormal/'
+
+x_data_normal = []
+y_data_normal =[]
+x_data_abnormal = []
+y_data_abnormal =[]
+
+files = glob.glob(pathnormal + '/*.wav')
+for file in files:
+    x_data_normal.append(preprocess.extract_features(file))
+    y_data_normal.append([1,0])
+    print(file)
+
+files = glob.glob(pathabnormal + '/*.wav')
+for file in files:
+    x_data_abnormal.append(preprocess.extract_features(file))
+    y_data_abnormal.append([0,1])
+    print(file)
+
+
+print("shape x normal data:", np.shape(x_data_normal) )
+print("shape y normal data:", np.shape(y_data_normal) )
+print("shape x abnormal data:", np.shape(x_data_abnormal)  )
+print("shape y abnormal data:", np.shape(y_data_abnormal)  )
+
+
+data_x = np.concatenate((x_data_normal,x_data_abnormal))
+data_y = np.concatenate((y_data_normal,y_data_abnormal))
+
+print("shape xdata:", np.shape(data_x) )
+print("shape y data:", np.shape(data_y) )
+
+
+with open('physionet_MFCC40t_X.pkl', 'wb') as o:
+    pickle.dump(data_x, o, pickle.HIGHEST_PROTOCOL)
+
+with open('physionet_MFCC40t_Y.pkl', 'wb') as o:
+    pickle.dump(data_y, o, pickle.HIGHEST_PROTOCOL)
