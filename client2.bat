300070083f3e7c0c5eff5cc26dfa833eaa7b1c92
afroozrahmati <afr.rahmati@gmail.com>
2022-02-25 15:53:21 -0500

    Keep neccessary Information and delete temproray file, making project clean

--- client2.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-"c:\users\Afrooz\anaconda3\envs\Capstone\python.exe" "D:\UW\Final thesis\audio_classification\client.py" client2
-pause
--- server.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-"c:\users\Afrooz\anaconda3\envs\Capstone\python.exe" "D:\UW\Final thesis\audio_classification\server.py"
-pause
